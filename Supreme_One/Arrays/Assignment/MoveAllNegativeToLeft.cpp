eb60a6ea7f017733b95bbea3d97dc27d0cfee107
Ankit Sharma <asharma@dainsta.com>
2023-09-04 10:02:32 +0530

    Started supreme 2

---
+++ Supreme_One/Arrays/Assignment/MoveAllNegativeToLeft.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(){
+    vector<int> arr{1,2,-13,4,-5,6,-6,-30,-45,-56,-77,34,45,76,-76};
+
+    // int np = 0;
+    // int pp=0;
+
+    // while(pp<arr.size()){
+    //     if(arr[pp]<0){
+    //         swap(arr[pp],arr[np]);
+    //         np++;
+    //         pp++;
+    //     }else{
+    //         pp++;
+    //     }
+    // }
+
+    int low = 0;
+    int high = arr.size()-1;
+
+    while(low<high){
+        if(arr[low]<0){
+            low++;
+        }
+        else if (arr[high]>0){
+            high--;
+        }else{
+            swap(arr[low],arr[high]);
+            low++;
+            high--;
+        }
+    }
+
+    for(auto value: arr){
+        cout<< value<< " ";
+    }
+
+    return 0;
+}
--- Supreme_One/Arrays/Class 3/transpose.cpp
+++ Supreme_One/Arrays/Class 3/transpose.cpp
@@ -5,8 +5,8 @@ using namespace std;
 
 
 int main(){
-    vector<vector<int>> matrixA ={{3,2,1},{6,3,8},{7,4,8}};
-    vector<vector<int>> matrixB(3,vector<int>(3, 0));
+    vector<vector<int> > matrixA ={{3,2,1},{6,3,8},{7,4,8}};
+    vector<vector<int> > matrixB(3,vector<int>(3, 0));
 
     for(int i=0;i<3;i++){
         for(int j=0;j<3;j++){
