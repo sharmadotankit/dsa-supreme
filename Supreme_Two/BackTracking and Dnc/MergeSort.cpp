c18e658805d5c18a6fe9aa0dfae083b1704ffd1f
Ankit Sharma <asharma@dainsta.com>
2023-10-25 10:38:23 +0530

    Learned mergeSrot

--- Supreme_Two/BackTracking and Dnc/MergeSort.cpp
+++ Supreme_Two/BackTracking and Dnc/MergeSort.cpp
@@ -1,10 +1,3 @@
-//============================================================================
-// Name        : dsa-supreme-2.cpp
-// Author      : Ankit Sharma
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
 #include<iostream>
 #include<vector>
 
